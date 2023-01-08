7c45885677de8e972e8cfbe403b3b94ec6d2fa82
Shaharyar Ahmed <113702415+Shaharyar2310@users.noreply.github.com>
2023-01-08 19:37:56 +0400

    vending machine
    done

---
+++ vending machine/vending machine.py
@@ -0,0 +1,560 @@
+ordered_items=[]
+items = [##menu list
+    {                           ##list contaning multiple dictionaries  
+        'code':1,               ##Code oe index number 
+        'name':'OASIS water',   ##Name of thing
+        'price':1,              ##Price of thing
+        'quantity':1            ##Aount avalible
+    },                          ##same in all dictionaries
+    {
+        'code':2,
+        'name':'Masafi water',
+        'price':1,
+        'quantity':5
+    },
+    {
+        'code':3,
+        'name':'Al Ain Water',
+        'price':1,
+        'quantity':5
+    },
+    {
+        'code':4,
+        'name':'Mai Dubai Water',
+        'price':1,
+        'quantity':5
+    },
+    {
+        'code':5,
+        'name':'Red Bull',
+        'price':15,
+        'quantity':5
+    },
+    {
+        'code':6,
+        'name':'Monster Energy',
+        'price':15,
+        'quantity':5
+    },
+    {
+        'code':7,
+        'name':'7up',
+        'price':2,
+        'quantity':5
+    },
+    {
+        'code':8,
+        'name':'Coca-Cola',
+        'price':4,
+        'quantity':5
+    },
+    {
+        'code':9,
+        'name':'Diet Coke',
+        'price':4,
+        'quantity':5
+    },
+    {
+        'code':10,
+        'name':'Coke Zero',
+        'price':4,
+        'quantity':5
+    },
+    {
+        'code':11,
+        'name':'Pepsi',
+        'price':4,
+        'quantity':5
+    },
+    {
+        'code':12,
+        'name':'Diet Pepsi',
+        'price':4,
+        'quantity':5
+    },
+    {
+        'code':13,
+        'name':'Mountain Dew',
+        'price':4,
+        'quantity':5
+
+    },
+    {
+        'code':14,
+        'name':'Sprite',
+        'price':4,
+        'quantity':5
+
+    },
+    {
+        'code':15,
+        'name':'Potato Chips',
+        'price':2,
+        'quantity':5
+    
+    },
+            {
+        'code':16,
+        'name':'Potato Sticks',
+        'price':2,
+        'quantity':5
+    },
+                {
+        'code':17,
+        'name':'Takis ',
+        'price':2,
+        'quantity':5
+    },
+                {
+        'code':18,
+        'name':'Tortilla Chips',
+        'price':2,
+        'quantity':5
+    },
+                {
+        'code':19,
+        'name':'Cheetos',
+        'price':2,
+        'quantity':5
+    },
+                {
+        'code':20,
+        'name':'Salad chips',
+        'price':2,
+        'quantity':5
+    },
+    {
+        'code':21,
+        'name':'Mars',
+        'price':2,
+        'quantity':5
+    },
+                {
+        'code':22,
+        'name':'Galaxy',
+        'price':2,
+        'quantity':5
+    },
+                {
+        'code':23,
+        'name':'Snickers',
+        'price':2,
+        'quantity':5
+    },
+                    {
+        'code':24,
+        'name':'Bounty',
+        'price':2,
+        'quantity':5
+    },
+                    {
+        'code':25,
+        'name':'M&M\'s',
+        'price':2,
+        'quantity':5
+    },
+                    {
+        'code':26,
+        'name':'Skittles',
+        'price':2,
+        'quantity':5
+    },
+                    {
+        'code':27,
+        'name':'Nuts',
+        'price':2,
+        'quantity':5
+    },                    {
+        'code':28,
+        'name':'Tea',
+        'price':2,
+        'quantity':5
+    },
+                        {
+        'code':29,
+        'name':'Green tea',
+        'price':2,
+        'quantity':5
+    },
+                        {
+        'code':30,
+        'name':'Lemon tea',
+        'price':2,
+        'quantity':5
+    },                        {
+        'code':31,
+        'name':'Latte',
+        'price':2,
+        'quantity':5
+    },
+                        {
+        'code':32,
+        'name':'Cappuccino',
+        'price':2,
+        'quantity':5
+    },
+                        {
+        'code':33,
+        'name':'Americano',
+        'price':2,
+        'quantity':5
+    },
+                            {
+        'code':34,
+        'name':'Espresso',
+        'price':2,
+        'quantity':5
+    },
+                            {
+        'code':35,
+        'name':'Mocha',
+        'price':2,
+        'quantity':5
+    }
+]
+water = [##menu list
+    {                           ##list contaning multiple dictionaries  
+        'code':1,               ##Code oe index number 
+        'name':'OASIS water',   ##Name of thing
+        'price':1,              ##Price of thing
+        'quantity':1            ##Aount avalible
+    },                          ##same in all dictionaries
+    {
+        'code':2,
+        'name':'Masafi water',
+        'price':1,
+        'quantity':5
+    },
+    {
+        'code':3,
+        'name':'Al Ain Water',
+        'price':1,
+        'quantity':5
+    },
+    {
+        'code':4,
+        'name':'Mai Dubai Water',
+        'price':1,
+        'quantity':5
+    }]
+
+energy_drinks =[{
+        'code':5,
+        'name':'Red Bull',
+        'price':15,
+        'quantity':5
+    },
+    {
+        'code':6,
+        'name':'Monster Energy',
+        'price':15,
+        'quantity':5
+    }]
+
+cold_drinks=[{
+        'code':7,
+        'name':'7up',
+        'price':2,
+        'quantity':5
+    },
+    {
+        'code':8,
+        'name':'Coca-Cola',
+        'price':4,
+        'quantity':5
+    },
+    {
+        'code':9,
+        'name':'Diet Coke',
+        'price':4,
+        'quantity':5
+    },
+    {
+        'code':10,
+        'name':'Coke Zero',
+        'price':4,
+        'quantity':5
+    },
+    {
+        'code':11,
+        'name':'Pepsi',
+        'price':4,
+        'quantity':5
+    },
+    {
+        'code':12,
+        'name':'Diet Pepsi',
+        'price':4,
+        'quantity':5
+    },
+    {
+        'code':13,
+        'name':'Mountain Dew',
+        'price':4,
+        'quantity':5
+
+    },
+    {
+        'code':14,
+        'name':'Sprite',
+        'price':4,
+        'quantity':5
+
+    }]
+chips=[{
+        'code':15,
+        'name':'Potato Chips',
+        'price':2,
+        'quantity':5
+    
+    },
+            {
+        'code':16,
+        'name':'Potato Sticks',
+        'price':2,
+        'quantity':5
+    },
+                {
+        'code':17,
+        'name':'Takis ',
+        'price':2,
+        'quantity':5
+    },
+                {
+        'code':18,
+        'name':'Tortilla Chips',
+        'price':2,
+        'quantity':5
+    },
+                {
+        'code':19,
+        'name':'Cheetos',
+        'price':2,
+        'quantity':5
+    },
+                {
+        'code':20,
+        'name':'Salad chips',
+        'price':2,
+        'quantity':5
+    }]
+Chocolate=[{
+        'code':21,
+        'name':'Mars',
+        'price':2,
+        'quantity':5
+    },
+                {
+        'code':22,
+        'name':'Galaxy',
+        'price':2,
+        'quantity':5
+    },
+                {
+        'code':23,
+        'name':'Snickers',
+        'price':2,
+        'quantity':5
+    },
+                    {
+        'code':24,
+        'name':'Bounty',
+        'price':2,
+        'quantity':5
+    },
+                    {
+        'code':25,
+        'name':'M&M\'s',
+        'price':2,
+        'quantity':5
+    },
+                    {
+        'code':26,
+        'name':'Skittles',
+        'price':2,
+        'quantity':5
+    },
+                    {
+        'code':27,
+        'name':'Nuts',
+        'price':2,
+        'quantity':5
+    }]
+Hot_drinks=[                    {
+        'code':28,
+        'name':'Tea',
+        'price':2,
+        'quantity':5
+    },
+                        {
+        'code':29,
+        'name':'Green tea',
+        'price':2,
+        'quantity':5
+    },
+                        {
+        'code':30,
+        'name':'Lemon tea',
+        'price':2,
+        'quantity':5
+    }]
+coffee=[                        {
+        'code':31,
+        'name':'Latte',
+        'price':2,
+        'quantity':5
+    },
+                        {
+        'code':32,
+        'name':'Cappuccino',
+        'price':2,
+        'quantity':5
+    },
+                        {
+        'code':33,
+        'name':'Americano',
+        'price':2,
+        'quantity':5
+    },
+                            {
+        'code':34,
+        'name':'Espresso',
+        'price':2,
+        'quantity':5
+    },
+                            {
+        'code':35,
+        'name':'Mocha',
+        'price':2,
+        'quantity':5
+    }
+]
+rows=[{'Name':'Water'},
+      {'Name':'Energy drinks'},
+      {'Name':'Cold drinks'},
+      {'Name':'Chips'},
+      {'Name':'Chocolate'},
+      {'Name':'Hot drinks'},
+      {'Name':'Coffee'}]
+Quit = False      ##to create a loop
+item = ''         ##Forgat what this was for(scared to remove it)
+
+print("-------\tWelcome to the vending machine\t-------")
+for i in items:
+      print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")
+while Quit == False:    ##Loop started 
+
+    money=0     ##to show balance or money
+    print("your balance is =",money)
+    
+    z=int(input("please enter the amount money : "))
+    money=z       ##code to input youe desired money
+    print("Your balance is :",money) 
+    
+    while True :
+      x=str(input("would you like to add more money (YES or NO) :"))
+                  ##loop to add more money(if you want to or added less)
+      if x == "Yes" or x == "yes" or x == "YES":   ##loop started if you say yes
+        Z=int(input("please enter the amount money : "))
+        money=z+Z     ##code to input youe desired money which will be added to total till you say no to above 
+        print("Your balance is : ",money)
+        z=money       ##To make the loop infinite 
+    
+      elif x == "No" or x == "no" or x == "NO":   ##If you say no the loop breaks
+        break
+
+    query = int(input("Enter the code number of the item you want to get: "))
+    for i in items:       ##Enter above the code of thing you would like to buy
+        if query == i['code']:
+            item = i
+                        ##This it to rename to make it easier for me
+            ordered_items.append({item['name']})
+                        ##Appendint the recipt
+            item["quantity"]-=1 
+                        ##For the stock or the amount availible
+    if item == '' :     ##If you type something wrong code
+        print('INVALID CODE')
+                        
+    if item["quantity"]<=-1:    ##To mantain stock
+        print('sorry out of this item')
+    
+    elif item['price']>money:   ##If invalid amount of money is added
+      print('insufficient change')
+      Y=str(input("press c to collect change: "))
+                                ##Collect unsed money 
+      if Y == "c" or Y == "C":
+          money=0
+          break
+          
+    elif item['price']<=money:   ##If money is of apporiate amount
+        print(f"Great, {item['name']} will cost you {item['price']} dollars")
+        X=money-item['price']
+        print("your change is,",X)
+        
+        Y=str(input("press c to collect change : "))                        ##Down below are just things to make code run smother
+        if Y == "c" or Y == "C":
+              money=0
+  
+
+        x=str(input("would you like to add something else (YES or NO) :"))
+                                ##looped to add things till you say no
+        if x == "Yes" or x == "yes" or x == "YES":
+          if query>=1 and query<=4:
+            print('May recomend these')
+            for i in chips:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")
+            for i in Chocolate:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")
+          if query>=5 and query<=6: 
+            print('May recomend these')   
+            for i in Chocolate:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")
+          if query>=7 and query<=14: 
+            print('May recomend these') 
+            for i in chips:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")
+            for i in Chocolate:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")
+          if query>=15 and query<=20: 
+            print('May recomend these') 
+            for i in water:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}") 
+            for i in energy_drinks:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")    
+            for i in cold_drinks:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")             
+          if query>=21 and query<=27: 
+            print('May recomend these')
+            for i in water:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}") 
+            for i in energy_drinks:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")    
+            for i in cold_drinks:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")
+            for i in chips:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")
+          if query>=28 and query<=30: 
+            print('May recomend these')
+            for i in chips:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")
+          if query>=31 and query<=35: 
+            print('May recomend these')
+            for i in chips:
+              print(f"Name: {i['name']}   -  Price: {i['price']}  -  code: {i['code']}")
+
+          Quit = False
+                                ##loop continued if you say yes
+        elif x == "No" or x == "no" or x == "NO":
+          Quit = True
+                                ##loop ended if you say no
+          print("thanks for buying")
+          rec=str(input("would you like a recept (yes or no) :"))
+                                ##code to get recipt
+          if rec == "Yes" or rec == "yes" or rec == "YES":
+              print(ordered_items)
+                                ##yes to get recipt
+          elif rec == "No" or rec == "no" or rec == "NO":
+              print("ok")
+                                ##No recept for you
+    
+    print('')
